34b853df14d43f67a6acf26594c950ae621b7fe1
unknown <shafaes@code.edu.az>
2022-07-07 12:33:08 +0400

    task10

---
+++ Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp__car_class
+{
+   public  class Car
+    {
+        public string brand;
+        public string price;
+        public string speed;
+        public string color;
+
+        public Car()
+        {
+            //Console.WriteLine("Default Constructor");
+
+        }
+        public Car(string brand, string price, string speed, string color) : this()
+
+        {
+            this.brand = brand;
+            this.price = price;
+            this.speed = speed;
+            this.color = color;
+            //Console.WriteLine("Custom constructor worked");
+        }
+        //public string GetDetails()
+        //{
+
+        //    return $"{brand} {price} {speed} {color}";
+        //}
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp__car_class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car BMW1 = new Car();
+            BMW1.brand = "BMW";
+            BMW1.price = "25000$";
+            BMW1.speed = "220km/h";
+            BMW1.color = "red";
+            Console.WriteLine($"{BMW1.brand}, {BMW1.price}, {BMW1.speed}, {BMW1.color}");
+
+            Car Mercedes1 = new Car();
+            Mercedes1.brand = "Mercedes";
+            Mercedes1.price = "50000$";
+            Mercedes1.speed = "300km/h";
+            Mercedes1.color = "black";
+            Console.WriteLine($"{Mercedes1.brand}, {Mercedes1.price}, {Mercedes1.speed}, {Mercedes1.color}");
+
+            //Console.WriteLine(BMW1.GetDetails());
+            //Console.WriteLine(Mercedes1.GetDetails());
+        }
+
+    }
+}
